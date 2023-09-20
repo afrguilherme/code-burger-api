887a1bfee60975f0f9554250ffc534ae822603b1
afrguilherme <augustogafr@gmail.com>
2023-09-20 08:54:55 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+class App {
+  constructor() {
+    this.app = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.app.use(express.json());
+  }
+
+  routes() {}
+}
