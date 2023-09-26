6bb5010bc02f9d86997afcd34c482ccbf68ba890
afrguilherme <augustogafr@gmail.com>
2023-09-26 15:07:25 -0300

    UserController development, data validation and catching data errors

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,35 @@
+import { v4 } from "uuid"
+import * as Yup from "yup"
+
+import User from "../models/Users"
+
+class UserController {
+  async store(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password_hash: Yup.string().required().min(6),
+      admin: Yup.boolean(),
+    })
+
+    try {
+      await schema.validateSync(request.body)
+    } catch (err) {
+      return response.status(400).json({ error: err.errors })
+    }
+
+    const { name, email, password_hash, admin } = request.body
+
+    const user = await User.create({
+      id: v4(),
+      name,
+      email,
+      password_hash,
+      admin,
+    })
+
+    return response.status(201).json({ id: user.id, name, email, admin })
+  }
+}
+
+export default new UserController()
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,9 @@
 import { Router } from "express"
-import { v4 } from "uuid"
-import User from "./app/models/Users"
+
+import UserController from "./app/controllers/UserController"
 
 const routes = new Router()
 
-routes.get("/", async (request, response) => {
-  const user = await User.create({
-    id: v4(),
-    name: "Guilherme",
-    email: "augustogafr@gmail.com",
-    password_hash: "123@",
-  })
-  return response.json(user)
-})
+routes.post("/users", UserController.store)
 
 export default routes
