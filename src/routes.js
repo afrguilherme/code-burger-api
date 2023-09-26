87c9e668a5f748c0f566d9bda9caffd8f123226a
afrguilherme <augustogafr@gmail.com>
2023-09-26 11:21:46 -0300

    User migration working with db

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from "express"
+import { v4 } from "uuid"
+import User from "./app/models/Users"
 
 const routes = new Router()
 
-routes.get("/", (request, response) => {
-  return response.json({ message: "Hello World!" })
+routes.get("/", async (request, response) => {
+  const user = await User.create({
+    id: v4(),
+    name: "Guilherme",
+    email: "augustogafr@gmail.com",
+    password_hash: "123@",
+  })
+  return response.json(user)
 })
 
 export default routes
