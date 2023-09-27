fa824e7101798df980aeae378fac5d6cd9956137
unknown <Administrator@dirad.intraer>
2023-09-27 14:53:42 -0300

    email already exists verification

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -13,12 +13,22 @@ class UserController {
     })
 
     try {
-      await schema.validateSync(request.body)
+      await schema.validateSync(request.body, { abortEarly: false })
     } catch (err) {
       return response.status(400).json({ error: err.errors })
     }
 
     const { name, email, password_hash, admin } = request.body
+
+    const userExists = await User.findOne({
+      where: { email },
+    })
+
+    if (userExists) {
+      return response
+        .status(400)
+        .json({ error: "User's email already exists!" })
+    }
 
     const user = await User.create({
       id: v4(),
