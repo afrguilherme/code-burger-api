46d05a928e49575ef702d21baf20f21af8cd2386
afrguilherme <augustogafr@gmail.com>
2023-10-03 13:40:56 -0300

    Products model added

--- src/routes.js
+++ src/routes.js
@@ -15,4 +15,6 @@ routes.post("/sessions", SessionController.store)
 
 routes.post("/products", upload.single("file"), ProductController.store)
 
+routes.get("/products", ProductController.index)
+
 export default routes
