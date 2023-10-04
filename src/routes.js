e0b2c8d01b036d59a9065796ca41e5447aa6cd1b
afrguilherme <augustogafr@gmail.com>
2023-10-04 13:38:48 -0300

    token verification added

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ import UserController from "./app/controllers/UserController"
 import SessionController from "./app/controllers/SessionController"
 import ProductController from "./app/controllers/ProductController"
 
+import authMiddleware from "./app/middlewares/auth"
+
 const upload = multer(multerConfig)
 const routes = new Router()
 
@@ -13,8 +15,8 @@ routes.post("/users", UserController.store)
 
 routes.post("/sessions", SessionController.store)
 
+routes.use(authMiddleware)
 routes.post("/products", upload.single("file"), ProductController.store)
-
 routes.get("/products", ProductController.index)
 
 export default routes
