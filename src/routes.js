9ae0dca795472eb790fe0b2520d31c736a556d93
afrguilherme <augustogafr@gmail.com>
2023-10-06 11:44:09 -0300

    OrderController development

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import ProductController from "./app/controllers/ProductController"
 import CategoryController from "./app/controllers/CategoryController"
 
 import authMiddleware from "./app/middlewares/auth"
+import OrderController from "./app/controllers/OrderController"
 
 const upload = multer(multerConfig)
 const routes = new Router()
@@ -22,5 +23,7 @@ routes.get("/products", ProductController.index)
 
 routes.post("/categories", CategoryController.store)
 routes.get("/categories", CategoryController.index)
+
+routes.post("/orders", OrderController.store)
 
 export default routes
